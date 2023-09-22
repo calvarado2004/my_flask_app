269d6e0d6198d40a8cb2175fe640c2670bd50e7c
calvarado2004 <carlos-alvarado@outlook.com>
2023-09-21 22:06:06 -0400

    adding html templates with bootstrap

--- src/app.py
+++ src/app.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
+import os
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../templates')
+
+app = Flask(__name__, template_folder=TEMPLATES_DIR)
 
 
 @app.route("/")
 def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+    return render_template("main.html")
 
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    return render_template("response.html", input_text=input_text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
